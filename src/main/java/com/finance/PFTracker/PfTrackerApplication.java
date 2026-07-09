a9b5016b11c5b5753101d9ae19f6a96e0bf62d58
Minh22042003 <duonghaiminh9a4@gmail.com>
2026-07-09 09:57:34 +0700

    My first commit

---
+++ src/main/java/com/finance/PFTracker/PfTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.finance.PFTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfTrackerApplication.class, args);
+	}
+
+}
